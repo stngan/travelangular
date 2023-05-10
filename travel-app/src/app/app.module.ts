1b01302b10b92055fe2ec02a1b191a2438a4920a
Kisad41 <116709089+Kisad41@users.noreply.github.com>
2023-05-10 14:21:41 +0700

    routing

--- travel-app/src/app/app.module.ts
+++ travel-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , RoutingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -116,7 +116,7 @@ import {ReactiveFormsModule } from '@angular/forms';
     DalatMapComponent,
     NhatrangMapComponent,
     VnMapComponent,
-
+    RoutingComponents
 
 
 
